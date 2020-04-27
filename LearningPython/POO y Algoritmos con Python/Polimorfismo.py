1cc5e8f4c75ae6430f6cd643adf0a7d0f8386274
Henry <henry.garcia@htlkeys.com>
2020-04-27 10:03:15 -0400

    Working with inheritance and polymorphism

---
+++ LearningPython/POO y Algoritmos con Python/Polimorfismo.py
@@ -0,0 +1,20 @@
+class Persona:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def avanza(self):
+        print('Ando Caminando')
+
+class Ciclista(Persona):
+
+    def __init__(self, nombre):
+        super().__init__(nombre)
+
+    def avanza(self):
+        print('Ando moviendome en bici')
+
+if __name__ == '__main__':
+    persona = Persona('Henry')
+    persona.avanza()
+    ciclista = Ciclista('HH')
+    ciclista.avanza()
